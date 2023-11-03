32e4bab1b52106009f949294563fbf8d12f185ff
iblas <ibrahimiblas@gmail.com>
2023-11-04 00:17:49 +0100

    Revert "python module"
    This reverts commit c4b17cc29fe3c0566ca674781a55d38451133f45.

--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    index = my_list[idx]
-    return index
